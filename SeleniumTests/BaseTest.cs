6a1e1cac8d125dfd8449dfb617a376bfbdef6ae5
Edgaras Velička <edgarasvelicka84@gmail.com>
2023-11-19 22:20:45 +0200

    Add BaseTest class

---
+++ SeleniumTests/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework.Interfaces;
+using NUnit.Framework;
+using SeleniumFramework;
+
+
+
+namespace SeleniumTests
+{
+    public class BaseTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.InitializeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+            {
+                string method = TestContext.CurrentContext.Test.MethodName;
+                string screenshotPathPath = Driver.TakeScreenshot(method);
+                TestContext.AddTestAttachment(screenshotPathPath);
+            }
+
+            Driver.QuitDriver();
+        }
+    }
+}
